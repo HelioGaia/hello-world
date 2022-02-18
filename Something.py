63d8ab2a301fa87541ee0a76db00173d445551e1
HelioGaia <99315352+HelioGaia@users.noreply.github.com>
2022-02-18 12:38:42 -0500

    Update Something.py

--- Something.py
+++ Something.py
@@ -1,9 +1,35 @@
 name = input("What is your name? ")
-favColor = input("What is your favorite Color?")
+color = input("What is your favorite Colour?")
 
+
+##Creates a "Random" String
+nC = 0
+cC = 0
+
+mixed = ""
+
+while nC < len(name) or cC < len(color):
+    try:
+        if nC * len(color) < cC * len(name):
+            mixed += name[nC]
+            nC += 1
+        else:
+            mixed += color[cC]
+            cC += 1
+    except:
+        break
+        
+        
 #Creates a value stored in num from ascii values that is to be added to the end of the password
 num = 0
 for i in range(len(name)):
     num += int(ord(name[i]))
-for i in range(len(favColor)):
-    num += int(or(favColor[i]))
+for i in range(len(color)):
+    num += int(ord(color[i]))
+
+    
+##Creates SPecial charecters
+spec = ""
+
+    
+print(mixed + str(num) + spec)
