bbbdedd7e7881d2ad47272fd13fac08a01203694
Petem4d <peter@54ds.net>
2022-02-18 12:26:53 -0500

    Creating number to add to end of password

--- Something.py
+++ Something.py
@@ -1,2 +1,9 @@
 name = input("What is your name? ")
-favColor = input("What is your favorite Colo(u)r?")
+favColor = input("What is your favorite Color?")
+
+#Creates a value stored in num from ascii values that is to be added to the end of the password
+num = 0
+for i in range(len(name)):
+    num += int(ord(name[i]))
+for i in range(len(favColor)):
+    num += int(or(favColor[i]))
